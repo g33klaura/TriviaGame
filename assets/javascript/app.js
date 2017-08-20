9fd729d8e294a9999340e838c81c5c037224ece9
Laura Boetcher <g33klaura@gmail.com>
2017-08-20 15:04:24 -0400

    Add start button function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,20 +3,20 @@
 
 
 /* VARIABLES =========================================
-*/
+ */
 
 // Object holding each question along with four possible answers
 var trivia = {
-	q1: ["In movie adaptation of The Hitchhiker\’s Guide to the Galaxy, who voiced Marvin the robot?", "Alan Rickman", "Alan Alda", "Alan Tudyk", "Alan Cumming"],
-	q2: ["Which member of Monty Python wrote and directed Brazil?", "Terry Gilliam", "John Cleese", "Graham Chapman", "Eric Idle"],
-	q3: ["Who played the villainous King Koopa in Super Mario Bros.?", "Dennis Hopper", "Dennis Leary", "Dennis Quaid", "Dennis Farina"],
-	q4: ["In Prometheus, who are the scientists in the crew searching for?", "The Engineers", "The Mechanics", "The Lumineers", "The Xenomorphs"],
-	q5: ["Who starred as engineer Dr. William Weir in Event Horizon?", "Sam Neill", "Sam Rami", "Sam Elliott", "Sam Rockwell"],
-	q6: ["What 80s pop star played the blood-thirsty Aunty Entity in Mad Max Beyond Thunderdome?", "Tina Turner", "Janet Jackson", "Vanessa Williams", "Gloria Estefan"],
-	q7: ["Jeff Bridges and Bruce Boxleitner, as Clu and Tron, battle what rogue AI operating system in Tron?", "Master Control Program", "Central Processing Unit", "Master Blaster", "Universal Serial Bus"],
-	q8: ["What is the name of the girl Caleb primarily interacts with in Ex Machina?", "Ava", "Eva", "Cher", "Anna"],
-	q9: ["In The Fifth Element, who plays Jean-Baptiste Emanuel Zorg?", "Gary Oldman", "Gary Sinise", "Gary Busey", "Gary Cole"],
-	q10: ["Who wrote and performed the title theme for Flash Gordon?", "Queen", "Kiss", "STYX", "Rush"]
+    q1: ["In movie adaptation of The Hitchhiker\’s Guide to the Galaxy, who voiced Marvin the robot?", "Alan Rickman", "Alan Alda", "Alan Tudyk", "Alan Cumming"],
+    q2: ["Which member of Monty Python wrote and directed Brazil?", "Terry Gilliam", "John Cleese", "Graham Chapman", "Eric Idle"],
+    q3: ["Who played the villainous King Koopa in Super Mario Bros.?", "Dennis Hopper", "Dennis Leary", "Dennis Quaid", "Dennis Farina"],
+    q4: ["In Prometheus, who are the scientists in the crew searching for?", "The Engineers", "The Mechanics", "The Lumineers", "The Xenomorphs"],
+    q5: ["Who starred as engineer Dr. William Weir in Event Horizon?", "Sam Neill", "Sam Rami", "Sam Elliott", "Sam Rockwell"],
+    q6: ["What 80s pop star played the blood-thirsty Aunty Entity in Mad Max Beyond Thunderdome?", "Tina Turner", "Janet Jackson", "Vanessa Williams", "Gloria Estefan"],
+    q7: ["Jeff Bridges and Bruce Boxleitner, as Clu and Tron, battle what rogue AI operating system in Tron?", "Master Control Program", "Central Processing Unit", "Master Blaster", "Universal Serial Bus"],
+    q8: ["What is the name of the girl Caleb primarily interacts with in Ex Machina?", "Ava", "Eva", "Cher", "Anna"],
+    q9: ["In The Fifth Element, who plays Jean-Baptiste Emanuel Zorg?", "Gary Oldman", "Gary Sinise", "Gary Busey", "Gary Cole"],
+    q10: ["Who wrote and performed the title theme for Flash Gordon?", "Queen", "Kiss", "STYX", "Rush"]
 };
 
 // Score starts at 0; number of correct, incorrect, and skipped questions will update these variables
@@ -30,7 +30,7 @@ var triviaIndex = 0;
 // Array of questions  ***Redundant?***
 var triviaArray = [trivia.q1, trivia.q2, trivia.q3, trivia.q4, trivia.q5, trivia.q6, trivia.q7, trivia.q8, trivia.q9, trivia.q10];
 
-	// console.log(triviaArray);
+// console.log(triviaArray);
 
 
 // Testing question
@@ -44,20 +44,20 @@ console.log(trivia.q3[1]);
 
 
 /* FUNCTIONS =========================================
-*/
+ */
+
 
 // Function to loop through trivia questions
 // and display each possible answer in RANDOM order
 
 function renderQuestion() {
-	if (triviaIndex <= (triviaArray.length - 1)) {
-		// Display question and answers
-		console.log("Next question");
-	}
-	else {
-		// If no more questions, render game end screen with scores
-		console.log("Game over");
-	}
+    if (triviaIndex <= (triviaArray.length - 1)) {
+        // Display question and answers
+        console.log("Next question");
+    } else {
+        // If no more questions, render game end screen with scores
+        console.log("Game over");
+    }
 }
 
 // ^^^^Should this function have the timer mechanism to not load immediately on function call??
@@ -67,43 +67,49 @@ function renderQuestion() {
 
 
 /* MAIN PROCESS =========================================
-*/
+ */
 
 renderQuestion();
 
+// This code makes sure the JS doesn't run until the HTML is finished loading
+// How much of the process does this block need to contain?****
+$(document).ready(function) {
+
 // When do question/answer buttons render????
 
-// When the player presses the Start button, it will run the following function...CHANGE TO JQUERY BUTTON METHOD
-document.onkeyup = function(event) {
-// then .remove to take off the page when game starts?
+// When the player presses the Start button, it will run the following function
+	$("#start-button").on("click", function() { 
+    // then .remove to take off the page when game starts?****
+    console.log("Start button was clicked");
+    });
 
-	// If there are no more questions, stop the function
-	if (triviaIndex === triviaArray.length) {
-		return;
-	}
+    // If there are no more questions, stop the function
+    if (triviaIndex === triviaArray.length) {
+        return;
+    }
 
-	// Determine which answer button was selected****
-	var playerGuess = "";
+    // Determine which answer button was selected****
+    var playerGuess = "";
 
-	// After each answer is selected, a screen displays telling user if correct,
-	if (playerGuess === triviaArray[triviaIndex][1]) {
-		console.log("Correct");
-	
-	// incorrect,
-	} else if (playerGuess !== triviaArray[triviaIndex][1]) {
-		console.log("Incorrect");
-	
-	// or if time ran out (these will be totaled at the END of the game)
-	} else {
-		console.log("Times up");
-	}
+    // After each answer is selected, a screen displays telling user if correct,
+    if (playerGuess === triviaArray[triviaIndex][1]) {
+        console.log("Correct");
 
-	// Increment the trivia question index variable and call the renderQuestion function
-	triviaIndex++;
-	renderQuestion();
+        // incorrect,
+    } else if (playerGuess !== triviaArray[triviaIndex][1]) {
+        console.log("Incorrect");
+
+        // or if time ran out (these will be totaled at the END of the game)
+    } else {
+        console.log("Times up");
+    }
+
+    // Increment the trivia question index variable and call the renderQuestion function
+    triviaIndex++;
+    renderQuestion();
 
 };
- 
+
 
 // After a set amount of time, the next question and answer set will appear
 
