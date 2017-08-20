7678827c9a72a3f32ad132ad1027bd3454710b8f
Laura Boetcher <g33klaura@gmail.com>
2017-08-19 21:55:20 -0400

    Initial variables and steps for game moved to readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # TriviaGame
 Test your knowledge with my JavaScript &amp; jQuery game!
+
+# Rules/ Process of the Game
+
+#// Will need to revise these steps to read more like a story*****
+
+// First screen shows start button to start game
+// One question with 4 possible answers shows with timer for each question
+// After answer selection, screen shows either correct, incorrect, or time out
+// After a few seconds, next question loads with new timer and 4 new answers
+// Repeats until number of questions is over
---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// Write function that doesn't have to be hard-coded, so can cycle through questions with answers?
+// Will those each be objects?
+
+
+/* VARIABLES =========================================
+*/
+
+// Object holding each question along with four possible answers
+var trivia = {
+	q1: ["In movie adaptation of 'The Hitchhiker\’s Guide to the Galaxy,' who voiced Marvin the robot?", "Alan Rickman", "Alan Alda", "Alan Tudyk", "Alan Cumming"];
+	q2: ["Which original member of Monty Python wrote and directed ‘Brazil’?", "Terry Gilliam", "John Cleese", "Graham Chapman", "Eric Idle"];
+	q3: ["Who played the villainous King Koopa in ‘Super Mario Bros.’?", "Dennis Hopper", "Dennis Leary", "Dennis Quaid", "Dennis Farina"];
+	q4: ["In ‘Prometheus,’ who are the scientists in the crew searching for?", "The Engineers", "The Mechanics", "The Lumineers", "The Xenomorphs"];
+	q5: ["Who starred as engineer Dr. William Weir in ‘Event Horizon’?", "Sam Neill", "Sam Rami", "Sam Elliott", "Sam Rockwell"];
+	q6: ["What 80s pop star played the blood-thirsty Aunty Entity in ‘Mad Max Beyond Thunderdome'?", "Tina Turner", "Janet Jackson", "Vanessa Williams", "Gloria Estefan"];
+	q7: ["Jeff Bridges and Bruce Boxleitner, as Clu and Tron, battle what rogue AI operating system in ‘Tron'?", "Master Control Program", "Central Processing Unit", "Master Blaster", "Universal Serial Bus"];
+	q8: ["What is the name of the girl Caleb mainly interacts with in ‘Ex Machina’?", "Ava", "Eva", "Cher", "Anna"];
+	q9: ["In ‘The Fifth Element,’ who plays Jean-Baptiste Emanuel Zorg?", "Gary Oldman", "Gary Sinise", "Gary Busey", "Gary Cole"];
+	q10: ["Who wrote and performed the title theme for ‘Flash Gordon'?", "Queen", "Kiss", "STYX", "Rush"];
+}
+
+// Score starts at 0; number of correct, incorrect, and skipped questions will update these variables
+var right = 0;
+var wrong = 0;
+var skipped = 0;
+
+// Variable to hold the index of current question
+var triviaIndex = 0;
+
+
+
+
+/* For total game ending screen, say something like: 
+Congrats, you\'ve scored ______. You like a lot of the same movies as me!
+*/
