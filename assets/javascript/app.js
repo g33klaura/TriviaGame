afd2b8b1249db9d60f313e1d340414ebe1d50c19
Laura Boetcher <g33klaura@gmail.com>
2017-08-19 23:18:55 -0400

    Add skeleton functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,10 +27,18 @@ var skipped = 0;
 // Variable to hold the index of current question
 var triviaIndex = 0;
 
-// Array of questions
+// Array of questions  ***Redundant?***
 var triviaArray = [trivia.q1, trivia.q2, trivia.q3, trivia.q4, trivia.q5, trivia.q6, trivia.q7, trivia.q8, trivia.q9, trivia.q10];
 
 	// console.log(triviaArray);
+
+
+// Testing question
+console.log(trivia.q3[0]);
+// Testing answers retrieval
+console.log(trivia.q3[1]);
+// console.log(triviaArray[3][1]);
+// Looks like either of these methods work....
 
 
 
@@ -38,17 +46,68 @@ var triviaArray = [trivia.q1, trivia.q2, trivia.q3, trivia.q4, trivia.q5, trivia
 /* FUNCTIONS =========================================
 */
 
-// Will need if/else for each correct answer
+// Function to loop through trivia questions
+// and display each possible answer in RANDOM order
+
+function renderQuestion() {
+	if (triviaIndex <= (triviaArray.length - 1)) {
+		// Display question and answers
+		console.log("Next question");
+	}
+	else {
+		// If no more questions, render game end screen with scores
+		console.log("Game over");
+	}
+}
+
+// ^^^^Should this function have the timer mechanism to not load immediately on function call??
+
+// Will need if/else for each correct answer?
 // Do every correct answer in one if (use || for or)?
 
-// Testing question
-console.log(trivia.q2[0]);
-// Testing answers retrieval
-console.log(trivia.q2[1]);
-console.log(triviaArray[1][1]);
-// Looks like either of these methods work....
 
-// When buttons for answers generate, they need to be in a random order
+/* MAIN PROCESS =========================================
+*/
+
+renderQuestion();
+
+// When do question/answer buttons render????
+
+// When the player presses the Start button, it will run the following function...CHANGE TO JQUERY BUTTON METHOD
+document.onkeyup = function(event) {
+// then .remove to take off the page when game starts?
+
+	// If there are no more questions, stop the function
+	if (triviaIndex === triviaArray.length) {
+		return;
+	}
+
+	// Determine which answer button was selected****
+	var playerGuess = "";
+
+	// After each answer is selected, a screen displays telling user if correct,
+	if (playerGuess === triviaArray[triviaIndex][1]) {
+		console.log("Correct");
+	
+	// incorrect,
+	} else if (playerGuess !== triviaArray[triviaIndex][1]) {
+		console.log("Incorrect");
+	
+	// or if time ran out (these will be totaled at the END of the game)
+	} else {
+		console.log("Times up");
+	}
+
+	// Increment the trivia question index variable and call the renderQuestion function
+	triviaIndex++;
+	renderQuestion();
+
+};
+ 
+
+// After a set amount of time, the next question and answer set will appear
+
+// This will repeat for the length of the trivia array
 
 
 
