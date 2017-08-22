dcc4be51c6a03336502bb06de7be83ee30fd8cf5
Laura Boetcher <g33klaura@gmail.com>
2017-08-21 23:39:27 -0400

    Remove commented-out bits to streamline parts that are working and see what's lacking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@
  */
 
 // Object holding each question along with four possible answers
+/*
 var trivia = {
     q1: ["In movie adaptation of The Hitchhiker\’s Guide to the Galaxy, who voiced Marvin the robot?", "Alan Rickman", "Alan Alda", "Alan Tudyk", "Alan Cumming"],
     q2: ["Which member of Monty Python wrote and directed Brazil?", "Terry Gilliam", "John Cleese", "Graham Chapman", "Eric Idle"],
@@ -18,6 +19,43 @@ var trivia = {
     q9: ["In The Fifth Element, who plays Jean-Baptiste Emanuel Zorg?", "Gary Oldman", "Gary Sinise", "Gary Busey", "Gary Cole"],
     q10: ["Who wrote and performed the title theme for Flash Gordon?", "Queen", "Kiss", "STYX", "Rush"]
 };
+*/
+
+// edit this one
+var trivia = {
+
+    q1: ["In movie adaptation of The Hitchhiker\’s Guide to the Galaxy, who voiced Marvin the robot?"],
+    q1a: ["Alan Rickman", "Alan Alda", "Alan Tudyk", "Alan Cumming"],
+    
+    q2: ["Which member of Monty Python wrote and directed Brazil?"],
+    q2a: ["Terry Gilliam", "John Cleese", "Graham Chapman", "Eric Idle"],
+
+    q3: ["Who played the reptilian villain, King Koopa, in Super Mario Bros.?"],
+    q3a: ["Dennis Hopper", "Dennis Leary", "Dennis Quaid", "Dennis Farina"],
+
+    q4: ["In Prometheus, who are the scientists in the crew searching for?"],
+    q4a: ["The Engineers", "The Mechanics", "The Lumineers", "The Xenomorphs"],
+
+    q5: ["Who starred as engineer Dr. William Weir in Event Horizon?"],
+    q5a: ["Sam Neill", "Sam Rami", "Sam Elliott", "Sam Rockwell"],
+
+    q6: ["What 80s pop star played the blood-thirsty Aunty Entity in Mad Max Beyond Thunderdome?"],
+    q6a: ["Tina Turner", "Janet Jackson", "Vanessa Williams", "Gloria Estefan"],
+
+    q7: ["Jeff Bridges and Bruce Boxleitner, as Clu and Tron, battle what rogue AI operating system in Tron?"],
+    q7a: ["Master Control Program", "Central Processing Unit", "Master Blaster", "Universal Serial Bus"],
+
+    q8: ["What is the name of the girl Caleb primarily interacts with in Ex Machina?"],
+    q8a: ["Ava", "Eva", "Cher", "Anna"],
+
+    q9: ["In The Fifth Element, who plays Jean-Baptiste Emanuel Zorg?"],
+    q9a: ["Gary Oldman", "Gary Sinise", "Gary Busey", "Gary Cole"],
+
+    q10: ["Who wrote and performed the title theme for Flash Gordon?"],
+    q10a: ["Queen", "Kiss", "STYX", "Rush"]
+
+};
+
 
 // Score starts at 0; number of correct, incorrect, and skipped questions will update these variables
 var right = 0;
@@ -25,10 +63,14 @@ var wrong = 0;
 var skipped = 0;
 
 // Variable to hold the index of current question
-var triviaIndex = 0;
+// setting this to 10 for now if decrementing the answers below****
+var triviaIndex = 10;
 
-// Array of questions  ***Redundant?***
+// Array of questions
 var triviaArray = [trivia.q1, trivia.q2, trivia.q3, trivia.q4, trivia.q5, trivia.q6, trivia.q7, trivia.q8, trivia.q9, trivia.q10];
+
+// Array of answers
+var answers = [trivia.q1a, trivia.q2a, trivia.q3a, trivia.q4a, trivia.q5a, trivia.q6a, trivia.q7a, trivia.q8a, trivia.q9a, trivia.q10a];
 
 // console.log(triviaArray);
 
@@ -40,45 +82,39 @@ var playerGuess = 0;
 // Testing question
 // console.log(trivia.q4[0]);
 // Testing answers retrieval
-// console.log(trivia.q4[1]);
+// console.log(trivia.q1a);
 // [1] is always right answer. Thus need these to populate randomly in buttons....
 
 
 
 
 /* FUNCTIONS =========================================
- */
+*/
 
 
 // Function to loop through trivia questions
 // and display each possible answer in RANDOM order
 
 
-// ****Still needs work!!!!!!!!!!
-// Nothing populates questions/answers. Start in console!
-function renderQuestion() {
+// Took out function renderQuestion() until ready for it
+// HERE******
+// renderQuestion needs to also a) render SHUFFLED answers, b) start/reveal a timer
 
-	// $("#qna").add().addClass("widget").appendTo(document.body);
-	
-	// for (triviaIndex = 0; triviaIndex < triviaArray.length; triviaIndex++) {
+function shuffle(array) {
+		var currentIndex = answers.length, tempValue, randomIndex;
 
-	// for (triviaIndex = 0; triviaIndex < trivia.length; triviaIndex++) {
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
 
-
-    	if (triviaIndex <= (triviaArray.length - 1)) {
-        // Display question and answers
-        	console.log(triviaArray[triviaIndex][0]);
-        	// console.log("Next question");
-   		
-   		} else {
-        // If no more questions, render game end screen with scores
-        	console.log("Game over");
-        	// Show score totals
-    	}
-
-	// }
-
+			tempValue = answers[currentIndex];
+			answers[currentIndex] = answers[randomIndex];
+			answers[randomIndex] = tempValue;
+		}
+	return array;
 }
+
+
 
 // ^^^^Should this function have the timer mechanism to not load immediately on function call??
 
@@ -87,29 +123,28 @@ function renderQuestion() {
 
 
 /* MAIN PROCESS =========================================
- */
-
+*/
 
 
 // This code makes sure the JS doesn't run until the HTML is finished loading
 // How much of the process does this block need to contain?****
 $(document).ready(function() {
 
+	// Starts off hiding portion that shows the answer buttons
 	$(".answers").hide();
 
-// When the player clicks the start button, it will run the following function to hide the start button and show the question/answer buttons
+	// When the player clicks the start button, this function runs to a) hide the start button, b) reveal the answer buttons and begin the game on the first question
 	$("#game-start").on("click", function() { 
     
-    console.log("Start button was clicked");
+    	console.log("Start button was clicked");
 
-    // then .hide to take off the page when game starts
-    // Not working on deployed GithubPages site tho..........
-    $("#game-start").hide();
+    	// Not working on deployed GithubPages site tho..........
+    	$("#game-start").hide();
 
-    $(".answers").show();
+    	$(".answers").show();
 
-    // Call a function to start rendering questions??
-    // renderQuestion();
+    	// Or really, the .show() for ".answers" should happen as part of the renderQuestion() function
+    	// renderQuestion();
     });
 
 
@@ -138,10 +173,11 @@ $(document).ready(function() {
     console.log("You chose: " + playerGuess);
 
     // After each answer is selected, a screen displays telling user if correct,
-    if (playerGuess === triviaArray[triviaIndex][1]) {
+    if (playerGuess === "Alan Rickman" || playerGuess === "Sam Neill" || playerGuess === "Tina Turner" || playerGuess === "Master Control Program" || playerGuess === "Ava" || playerGuess === "The Engineers" || playerGuess === "Queen" || playerGuess === "Gary Oldman" || playerGuess === "Terry Gilliam" || playerGuess === "Dennis Hopper") {
+
         console.log("Correct");
 
-        // incorrect,
+        // incorrect,  ***BUT THIS WON"T WORK IF RANDOMIZING THE QUESTIONS
     } else if (playerGuess !== triviaArray[triviaIndex][1]) {
         console.log("Incorrect");
 
@@ -150,9 +186,9 @@ $(document).ready(function() {
         console.log("Times up");
     }
 
-    // Increment the trivia question index variable and call the renderQuestion function
-    triviaIndex++;
-    renderQuestion();
+    // Increment or DECREMENT**** (depends on how being used above) the trivia question index variable and call the renderQuestion function
+    triviaIndex--;
+    // renderQuestion();
 
 });
 
@@ -163,10 +199,8 @@ $(document).ready(function() {
 
 
 
+// For total game ending screen, say something like: 
+// Congrats, you\'ve scored ______. You like a lot of the same movies as me!
 
-
-/* For total game ending screen, say something like: 
-Congrats, you\'ve scored ______. You like a lot of the same movies as me!
-*/
 
 // Want Flash Theme to play on end screen!!!!
