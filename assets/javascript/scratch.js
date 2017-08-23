675cee8f5226c9180f22218d06b3460c143b3dc0
Laura Boetcher <g33klaura@gmail.com>
2017-08-23 15:23:52 -0400

    Add more variations of shuffle function

--- assets/javascript/scratch.js
+++ assets/javascript/scratch.js
@@ -1,6 +1,7 @@
 /* Place for bits and pieces of things tried and in progress
 */
 
+// As long as this isn't linked to the html, nothing should interact
 
 
 // IN-PROGRESS FUNCTIONS
@@ -29,3 +30,52 @@ function renderQuestion() {
 
 	}
 
+// ^^^^Should this function have the timer mechanism to not load immediately on function call??
+
+// Tried this guy: would only shuffle "top layer", not the answers ~within~ the array
+function shuffle(array) {
+		var currentIndex = answers.length, tempValue, randomIndex;
+
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			tempValue = answers[currentIndex];
+			answers[currentIndex] = answers[randomIndex];
+			answers[randomIndex] = tempValue;
+		}
+	return array;
+}
+
+/*
+function shuffle(array) {
+		while (randomArray.length < 4) {
+			var rand = Math.floor(Math.random() * 4);
+
+			if (randomArray.indexOf(trivia[currentQuestion].answers[0][rand]) === -1) {
+				randomArray.push(trivia[currentQuestion].answers[0][rand]);
+			}
+		}
+	console.log(randomArray);
+}
+*/
+
+
+
+
+// <!-- Piece that Eric showed me -->
+	function randomizearray() {
+		 while (ranarray.length < 4){ 
+		 	var rand = Math.floor(Math.random() * 4);
+		 	if (ranarray.indexOf(trivia[round].ansarray[rand]) === -1) {
+		 	ranarray.push(trivia[round].ansarray[rand]);
+		 }
+		}
+		 console.log(ranarray);
+	}
+
+
+// Eric helping from Slack
+	console.log(array[i].object[j])
+	where i is the index (0,1,2,3etc) and j is the index of the object
+	in your case console.log(trivia[0].q1a[0]) should return ALan Rickman
